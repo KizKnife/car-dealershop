ff01424262ca78f346ba53024443afff5fb5509a
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-05-08 14:46:22 -0500

    Feat: Dealership.java, UserInterface.java, Vehicle.java

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(double min, double max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(double min, double max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class UserInterface {
+    Dealership dealership = new Dealership();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void UserInterface() {
+
+    }
+
+    public void display() {
+
+    }
+
+
+}
