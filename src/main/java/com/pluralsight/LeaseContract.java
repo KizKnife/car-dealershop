d86ff08c63619f46848bd9585c681c1dced19a7b
Yumozh <juliiamozhchil@gmail.com>
2026-05-17 23:09:58 -0500

    calculate total price with all fees for lease

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class LeaseContract extends Contract{
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String customerName, String email, Vehicle vehicleSold, double expectedEndingValue, double leaseFee) {
+        super(date, customerName, email, vehicleSold);
+        this.setExpectedEndingValue();
+        this.setLeaseFee();
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue() {
+        this.expectedEndingValue = this.getVehicleSold().getPrice() * 0.5;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee() {
+        this.leaseFee = this.getVehicleSold().getPrice() * 0.07;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return this.getMonthlyPayment() * 36 + this.leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return ((this.getVehicleSold().getPrice() * 0.04) + this.getVehicleSold().getPrice()) / 36;
+    }
 }
